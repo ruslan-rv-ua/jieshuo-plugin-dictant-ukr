58fc31594918608bfeb221738b5baa08c0ed48a3
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2023-09-07 11:22:04 +0300

    chore: fix .gitignore

--- build.py
+++ build.py
@@ -7,11 +7,11 @@ FILES_TO_ZIP = ["src/main.lua"]
 current_dir = Path(__file__).parent.absolute()
 plugin = tomllib.loads((current_dir / "project.toml").read_text(encoding="utf-8"))
 
-build_dir = current_dir / "build"
+build_dir = current_dir / "dist"
 if not build_dir.exists():
     build_dir.mkdir()
 zip_file_name = f"{plugin['build_name']}-{plugin['version']}-{plugin['language']}.ppk"
 
-with zipfile.ZipFile(current_dir/ "build" / zip_file_name, "w", zipfile.ZIP_DEFLATED, compresslevel=9) as zipf:
+with zipfile.ZipFile(build_dir / zip_file_name, "w", zipfile.ZIP_DEFLATED, compresslevel=9) as zipf:
     for file_to_zip in FILES_TO_ZIP:
         zipf.write(file_to_zip) 
